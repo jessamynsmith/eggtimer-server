88be24b6f5103aea53c3e7647dc50c121aa8fd52
Jessamyn Smith <geekchick77@gmail.com>
2013-02-15 13:33:28 -0500

    Added south

--- egg_timer/settings.py
+++ egg_timer/settings.py
@@ -112,6 +112,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'egg_timer.apps.periods',
 )
 
 # A sample logging configuration. The only tangible logging
