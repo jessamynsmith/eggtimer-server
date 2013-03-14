aa3b32af64aaca83daf4db8032c17ecbd8ae3b7b
Jessamyn Smith <geekchick77@gmail.com>
2013-03-14 00:33:05 -0400

    Added more information to email text

--- egg_timer/apps/periods/management/commands/notify_upcoming_period.py
+++ egg_timer/apps/periods/management/commands/notify_upcoming_period.py
@@ -9,22 +9,29 @@ class Command(BaseCommand):
     help = 'Notify users of upcoming periods'
 
     def handle(self, *args, **options):
-        today = datetime.date.today()
         users = userprofile_models.UserProfile.objects.filter(
             periods__isnull=False).filter(statistics__isnull=False).distinct()
         for user in users:
-            last_period = user.periods.order_by('-start_date')[0]
-            days_since_last = (today - last_period.start_date).days
             if not user.statistics.average_cycle_length:
                 continue
-            expected_in = user.statistics.average_cycle_length - days_since_last
+            expected_in = user.statistics.average_cycle_length - user.statistics.current_cycle_length
+            subject = ''
+            if abs(expected_in) == 1:
+                day = 'day'
+            else:
+                day = 'days'
+            expected_date = datetime.date.today() + datetime.timedelta(days=expected_in)
+            formatted_date = expected_date.strftime('%B %d, %Y')
             if expected_in < 0:
-                subject = "Period was expected %s days ago" % (abs(expected_in))
-                text_body = "You should have gotten your period %s days ago. Did you forget to add your last period?" % (abs(expected_in))
-                send_email(user, subject, text_body, None)
+                subject = "Period was expected %s %s ago" % (abs(expected_in), day)
+                text_body = "You should have gotten your period %s %s ago, on %s. Did you forget to add your last period?" % (abs(expected_in), day, formatted_date)
+            elif expected_in == 0:
+                subject = "Period today!"
+                text_body = "You should be getting your period today!"
             elif expected_in < 4:
-                subject = "Period expected in %s days" % expected_in
-                text_body = "You should be getting your period in %s days." % expected_in
+                subject = "Period expected in %s %s" % (expected_in, day)
+                text_body = "You should be getting your period in %s %s, on %s." % (expected_in, day, formatted_date)
+            if subject:
                 send_email(user, subject, text_body, None)
 
 
