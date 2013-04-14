8e38d4da3f2ea27329837d1d42da011db6ab6d82
Jessamyn Smith <geekchick77@gmail.com>
2013-04-14 15:59:03 -0400

    Only add html if present

--- egg_timer/libs/email_sender.py
+++ egg_timer/libs/email_sender.py
@@ -4,5 +4,6 @@ from django.core.mail import EmailMultiAlternatives
 def send(recipient, subject, text_body, html_body):
     recipients = [(recipient.full_name, recipient.email)]
     msg = EmailMultiAlternatives(subject, text_body, to=recipients)
-    msg.attach_alternative(html_body, "text/html")
+    if html_body:
+        msg.attach_alternative(html_body, "text/html")
     msg.send()
