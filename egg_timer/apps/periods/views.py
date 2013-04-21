d9759483444b1f31996e1644034d0fd953702b22
Jessamyn Smith <geekchick77@gmail.com>
2013-04-21 12:25:47 -0400

    Simplified histogram code

--- egg_timer/apps/periods/views.py
+++ egg_timer/apps/periods/views.py
@@ -19,28 +19,29 @@ def statistics(request):
 
 
 def grouped(iterable, n):
-    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    """s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."""
     return izip(*[iter(iterable)]*n)
 
 
 @login_required
 def frequencies(request):
-    periods = period_models.Period.objects.filter(userprofile__user=request.user)
-    cycle_lengths = [x for x in periods.values_list('length', flat=True) if x is not None]
-    if not cycle_lengths:
+    periods = period_models.Period.objects.filter(
+        userprofile__user=request.user, length__isnull=False).order_by('length')
+    if len(periods) < 1:
         raise Http404
-    distinct = set(cycle_lengths)
-    in_order = sorted(cycle_lengths)
+
+    cycle_lengths = periods.values_list('length', flat=True)
+    shortest = cycle_lengths[0]
+    longest = cycle_lengths[len(cycle_lengths) - 1]
 
     fig = pyplot.figure()
     ax = fig.add_subplot(111)
 
     # the histogram of the data
-    bins = [x - 0.5 for x in distinct]
-    bins.append(bins[-1] + 1)
-    n, bins, patches = ax.hist(in_order, bins=bins)
+    bins = [x - 0.5 for x in range(shortest, longest + 2)]
+    n, bins, patches = ax.hist(cycle_lengths, bins=bins)
     y_range = n.max(0) + 1
-    pyplot.xticks(range(in_order[0], in_order[-1] + 1))
+    pyplot.xticks(range(shortest, longest + 1))
 
     y_step = int(math.ceil(y_range / 10.0))
     y_ticks = [x[0] for x in grouped(range(y_range), y_step)]
@@ -50,7 +51,7 @@ def frequencies(request):
     ax.set_xlabel('Cycle Length')
     ax.set_ylabel('Count')
     ax.set_title('Cycle Length Frequency')
-    ax.set_xlim(in_order[0] - 1.5, in_order[-1] + 1.5)
+    ax.set_xlim(shortest - 1.5, longest + 1.5)
     ax.set_ylim(0, y_range)
     ax.yaxis.grid(True)
 
