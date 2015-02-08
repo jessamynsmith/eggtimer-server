a82b6ae85754ba26040b2deb72d3c9a71894da2d
Jessamyn Smith <geekchick77@gmail.com>
2015-02-07 23:06:03 -0500

    Added setting of env vars to script

--- scripts/deploy_to_heroku.sh
+++ scripts/deploy_to_heroku.sh
@@ -5,9 +5,15 @@ set -e
 
 sh .git/hooks/pre-commit
 
+heroku config:set \
+ADMIN_EMAIL="egg.timer.app@gmail.com" \
+ADMIN_NAME="the egg timer" \
+DJANGO_SETTINGS_MODULE=eggtimer.settings.production\
+> /dev/null
+
 git push heroku master
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
-python manage.py syncdb
-python manage.py migrate
+python manage.py syncdb --noinput
+python manage.py migrate --noinput
