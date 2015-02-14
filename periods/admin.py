b5bb7d03b0b2f5ca310b2fd01fe213dab4a51f6a
Jessamyn Smith <geekchick77@gmail.com>
2015-02-13 22:34:39 -0500

    Use django-custom-user admin

--- periods/admin.py
+++ periods/admin.py
@@ -1,31 +1,11 @@
+from custom_user.admin import EmailUserAdmin
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
-from django.utils.translation import ugettext_lazy as _
 
-from periods import forms as period_forms, models
+from periods import models
 
 
-class UserAdmin(auth_admin.UserAdmin):
-    # The forms to add and change user instances
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference the removed 'username' field
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'birth_date',
-                                         'luteal_phase_length')}),
-        (_('Settings'), {'fields': ('send_emails',)}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = ((None, {'classes': ('wide',), 'fields': ('email', 'password1', 'password2')}),)
-    form = period_forms.UserChangeForm
-    add_form = period_forms.UserCreationForm
-    list_display = ('email', 'first_name', 'last_name', 'send_emails')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
+class UserAdmin(EmailUserAdmin):
+    pass
 
 
 admin.site.register(models.User, UserAdmin)
