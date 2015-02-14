120a8dc42bdd491345480a12ec0c6c75f58485e5
Jessamyn Smith <geekchick77@gmail.com>
2015-02-13 22:28:11 -0500

    Improved javascript test coverage

--- periods/tests/static/js/test_calendar.js
+++ periods/tests/static/js/test_calendar.js
@@ -15,12 +15,26 @@ describe('Array', function () {
             assert.equal(0, result.length);
         });
         it('should return array of items if data', function () {
-            var data = Array({id: '1', type: 'period', start_date: '2015-01-01'});
+            var data = Array(
+                {id: '1', type: 'period', start_date: '2015-01-01'},
+                {id: '2', type: 'projected period', start_date: '2015-01-28'},
+                {id: '3', type: 'projected ovulation', start_date: '2015-01-14'},
+                {id: '4', type: 'day count', start_date: '2015-01-01', text: 'Day: 1'}
+            );
             var result = makeEvents(data);
-            assert.equal(1, result.length);
-            assert.equal("period", result[0].title);
-            assert.equal("2015-01-01", result[0].start);
-            assert.equal("#0f76ed", result[0].color);
+            assert.equal(4, result.length);
+            var expected = JSON.stringify({title: "period", start: "2015-01-01",
+                "period_id": "1", color: "#0f76ed"});
+            assert.equal(JSON.stringify(result[0]), expected);
+            expected = JSON.stringify({title: "projected period", start: "2015-01-28",
+                "period_id": "2", color: "darkred"});
+            assert.equal(JSON.stringify(result[1]), expected);
+            expected = JSON.stringify({title: "projected ovulation", start: "2015-01-14",
+                "period_id": "3", color: "purple"});
+            assert.equal(JSON.stringify(result[2]), expected);
+            expected = JSON.stringify({title: "Day: 1", start: "2015-01-01", "period_id": "4",
+                color: "#ffffff", "textColor": "#666666", "editable": false});
+            assert.equal(JSON.stringify(result[3]), expected);
         });
     })
 });
