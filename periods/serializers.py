65c992398797c3c18b7f46f8372f3c2631a89261
Jessamyn Smith <geekchick77@gmail.com>
2015-03-01 15:57:22 -0500

    Use modified version of enumfield, that allows empty values. Switch from forked coverage plugin to coverage development version

--- periods/serializers.py
+++ periods/serializers.py
@@ -10,6 +10,13 @@ class FlowEventSerializer(serializers.ModelSerializer):
         model = period_models.FlowEvent
         exclude = ('user',)
 
+    def validate_clots(self, value):
+        # TODO submit patch to drf to handle nullable fields and coerce like Django does?
+        # Coerce empty values to None
+        if not value:
+            value = None
+        return value
+
 
 class FlowEventFilter(django_filters.FilterSet):
     min_timestamp = django_filters.DateTimeFilter(name="timestamp", lookup_type='gte')
