84320f83daa91f00408a9549342621c917b99da5
jessamynsmith <jessamyn.smith@gmail.com>
2018-03-30 16:00:57 -0600

    Added missing var declaration on js function

--- eggtimer/static/js/base.js
+++ eggtimer/static/js/base.js
@@ -1,4 +1,4 @@
-addFormStyles = function() {
+var addFormStyles = function() {
     $('select,input[type="text"],input[type="datetime"],input[type="date"],input[type="time"],input[type="number"]').addClass('form-control').addClass('my-form-control');
     $('label').addClass('control-label').addClass('my-control-label');
 };
