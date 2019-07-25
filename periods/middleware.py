a39cf9d55fd4b4d279a9155353043bb731c3ad27
jessamynsmith <jessamyn.smith@gmail.com>
2019-07-25 13:23:42 -0600

    Only set auth token if user exists on request

--- periods/middleware.py
+++ periods/middleware.py
@@ -3,7 +3,7 @@ class AddAuthTokenMiddleware(object):
         Adds auth_token cookie to response
     """
     def process_response(self, request, response):
-        if request.user.is_authenticated():
+        if hasattr(request, 'user') and request.user and request.user.is_authenticated():
             auth_token = request.user.auth_token
             if auth_token:
                 response.set_cookie('auth_token', auth_token)
